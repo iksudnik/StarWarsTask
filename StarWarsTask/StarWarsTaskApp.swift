8632345b74ed18507a487944ebd641d57e8f1b0d
Ilja Sudnik <ik.sudnik@gmail.com>
2025-03-08 19:19:36 +0100

    Initial Commit

---
+++ StarWarsTask/StarWarsTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsTaskApp.swift
+//  StarWarsTask
+//
+//  Created by Ilja Sudnik on 08.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
